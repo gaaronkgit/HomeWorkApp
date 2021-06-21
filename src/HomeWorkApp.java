2bfdf39b6f1c4f34b4f6a87f8a83b8bf6609f31a
Gaaronk <s.ivanov.kr23@gmail.com>
2021-06-21 23:53:13 +0500

    Add: Comment

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,6 +1,7 @@
 public class HomeWorkApp {
     public static void main (String[] args)
     {
+        //Выполнение методов
         printThreeWords();
         checkSumSign();
         printColor();
